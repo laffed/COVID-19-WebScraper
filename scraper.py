ed819351751efae02cf1236e8bf94f453189bce2
laffed <roarklaffed@gmail.com>
2020-03-05 14:19:14 -0600

    1. Added a loop to ensure the URL check will work no matter current date

--- scraper.py
+++ scraper.py
@@ -3,24 +3,34 @@
 from selenium import webdriver
 from datetime import date, timedelta
 import requests
+import csv
+import json
 import bs4 as bs
 import urllib.request
 import pandas as pd
 
-#driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
 today = date.today()
-yesterday = today - timedelta(days=1)
 todayFormatted = today.strftime("%m-%d-%Y")
-yesterdayFormatted = yesterday.strftime("%m-%d-%Y")
 
+daysToSubtract = 1
 r_today = requests.get(
     f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{todayFormatted}.csv")
-if (r_today.status_code == 200):
-    confirmedURL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{todayFormatted}.csv"
-else:
-    confirmedURL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{yesterdayFormatted}.csv"
 
+if(r_today.status_code != 200):
+    while (r_today.status_code != 200):
+        tryDate = today - timedelta(days=daysToSubtract)
+        tryDateFormatted = tryDate.strftime("%m-%d-%Y")
+        daysToSubtract += 1
+        r_today = requests.get(
+            f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{tryDateFormatted}.csv")
+
+    confirmedURL = (
+        f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{tryDateFormatted}.csv")
+else:
+    confirmedURL = (
+        f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{todayFormatted}.csv")
 # driver.get(confirmedURL)
 
 sauce = urllib.request.urlopen(confirmedURL).read()
